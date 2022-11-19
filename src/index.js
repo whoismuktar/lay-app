1558bc67532a94bfef9719cc29cafd4cfeefbe23
Muktar Yusuf <mayunited12@yahoo.com>
2022-11-19 22:12:42 +0100

    implment router directory

--- src/index.js
+++ src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import router from "./router"
+
+import {
+  RouterProvider,
+} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <RouterProvider router={router} />
     <App />
   </React.StrictMode>
 );
