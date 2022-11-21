a7b2a2f3a44623efe066507d30425b87c49ab3e6
Muktar Yusuf <mayunited12@yahoo.com>
2022-11-21 13:33:26 +0100

    migrate Feed own component

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,14 +1,6 @@
 import React from "react";
-import { Image, Divider, Tooltip } from "antd";
-import { Filter } from "../../Helper";
-import {GrDown} from "react-icons/gr"
-import {HiOutlineChevronRight} from "react-icons/hi"
-import {BsFillBookmarkFill, BsFlagFill, BsFillBellFill} from "react-icons/bs"
-import {TfiLink} from "react-icons/tfi"
-import {IoMdShareAlt} from "react-icons/io"
-import "../../Assets/styles/feed.scss";
+import Feed from "../../components/Feed"
 
-const appAvatar = require("../../Assets/images/avatar.jpg");
 const mainFeed = [
   {
     id: 1,
@@ -73,80 +65,7 @@ function Home(props) {
     <div className="home">
       <div className="feeds">
         {mainFeed.map((feed, i) => (
-          <div key={i} className="app-card feed feed-card">
-            <div className="feed__title">{feed.title}</div>
-
-            <div className="feed__top">
-              <div className="feed__asked_by_avatar">
-                <Image
-                  width={40}
-                  height={40}
-                  src={
-                    feed.askedBy.profileImage
-                      ? feed.askedBy.profileImage
-                      : appAvatar
-                  }
-                  alt={`${feed.askedBy.firstName}'s profile image`}
-                />
-              </div>
-
-              <div className="feed__details">
-                <div className="feed__asked_by_name">
-                  {`${feed.askedBy.firstName} ${feed.askedBy.lastName}`}
-                </div>
-                <div className="feed__meta">
-                  <div className="feed__answered_by">
-                    Answered by Mr A, Ms B & {17 + (i + 1)} more
-                  </div>
-
-                  <span class="bullet-separator">&bull;</span>
-
-                  <div className="feed__answered_date">
-                    {Filter.formatDateFromNowShort(feed.created_at)} ago
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div className="feed__description"
-              dangerouslySetInnerHTML={{__html:feed.description}}
-            />
-
-            {feed.image && (
-              <div className="feed__image">
-                <Image width={200} src={feed.image} />
-              </div>
-            )}
-
-            <div className="feed__icons">
-              <Tooltip title="Bookmark">
-                <BsFillBookmarkFill className="feed__icon" />
-              </Tooltip>
-              <Tooltip title="Copy Link">
-                <TfiLink className="feed__icon" />
-              </Tooltip>
-              <Tooltip title="Share">
-                <IoMdShareAlt className="feed__icon" />
-              </Tooltip>
-              <Tooltip title="Flag">
-                <BsFlagFill className="feed__icon" />
-              </Tooltip>
-              <Tooltip title="Subscribe">
-                <BsFillBellFill className="feed__icon" />
-              </Tooltip>
-            </div>
-
-            <div className="feed__answers">
-            <Divider orientation="center" plain className="qa-divider">
-              <div className="inner">
-                <span className="__text">Lay Answers</span>
-                <GrDown className="__icon" />
-                {/* <HiOutlineChevronRight className="__icon" /> */}
-              </div>
-            </Divider>
-
-            </div>
-          </div>
+          <Feed feed={feed} i={i} />
         ))}
       </div>
     </div>
