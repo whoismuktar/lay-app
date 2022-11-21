26849346418e6c55ee1d5c190d9728361e012d20
Muktar Yusuf <mayunited12@yahoo.com>
2022-11-21 04:31:45 +0100

    adopt new sass model

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-// import logo from './logo.svg';
-import './App.css';
-// import Layout from "./components/Layout"
+import './Assets/styles/App.scss';
 
 function App({children}) {
   return (
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './Assets/styles/index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import router from "./router"
