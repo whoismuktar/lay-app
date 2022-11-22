b2d258de88dceb730455c328de2a010c01638580
Muktar Yusuf <mayunited12@yahoo.com>
2022-11-22 09:08:41 +0100

    repogress on replies

--- src/components/ReplyThread/Reply.js
+++ src/components/ReplyThread/Reply.js
@@ -1,40 +1,57 @@
 import React from "react";
+import { AiOutlineHeart, AiTwotoneHeart } from "react-icons/ai";
+import { FiMoreHorizontal } from "react-icons/fi";
 import { Filter } from "../../Helper";
 import UserCard from "../UserCard";
 
 const recursiveReply = (reply) => {
-    return <Reply comment={reply} />
-}
+  return <Reply comment={reply} />;
+};
 function Reply({ comment }) {
   return (
-    <div className="comment">
-      <div className="comment__top align-center">
-        <UserCard user={comment.user} />
+    <>
+    <div className="align-center">
+      <div className="comment">
+        <div className="comment__top align-center">
+          <UserCard user={comment.user} />
 
-        <div>
-          <span className="user__name">
-            {Filter.getFullName(comment.user)}
-            {comment.replies && comment.replies.length}
-          </span>
-          <div className="feed__answered_date">
-            {Filter.formatDateFromNowShort(new Date())} ago
+          <div>
+            <span className="user__name">
+              {Filter.getFullName(comment.user)}
+              {comment.replies && comment.replies.length}
+            </span>
+            <div className="comment__answered_date comment__time">
+              {Filter.formatDateFromNowShort(new Date())} ago
+            </div>
           </div>
+        </div>
+
+        <div
+          className="comment__text"
+          dangerouslySetInnerHTML={{ __html: comment.comment.title }}
+        />
+
+        <div className="comment__action">
+          <span className="comment__reply">Reply</span>
+          <span className="comment__flag">Flag</span>
+        </div>
+
+        {/* {comment.replies && recursiveReply(comment.replies)} */}
+        {/* {JSON.stringify(comment)} */}
+      </div>
+
+      <div className="dflex comment__side-action">
+        <div className="comment__like">
+          {!comment.liked && <AiOutlineHeart />}
+          {comment.liked && <AiTwotoneHeart className="liked" />}
+        </div>
+        <div className="more-option">
+          <FiMoreHorizontal />
         </div>
       </div>
 
-      <div
-        className="feed__description"
-        dangerouslySetInnerHTML={{ __html: comment.comment.title }}
-      />
-
-      <div className="comment__action">
-        <span className="comment__reply">Reply</span>
-        <span className="comment__flag">Flag</span>
-      </div>
-
-      {/* {comment.replies && recursiveReply(comment.replies)} */}
-      {/* {JSON.stringify(comment)} */}
     </div>
+    </>
   );
 }
 
