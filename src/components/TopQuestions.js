016fd26c11e158a33c6f2a7f948ec23adc577463
Muktar Yusuf <mayunited12@yahoo.com>
2022-11-24 22:58:39 +0100

    global page title

--- src/components/TopQuestions.js
+++ src/components/TopQuestions.js
@@ -5,6 +5,7 @@ import "../Assets/styles/question.scss"
 function TopQuestions(props) {
   return (
     <div className="top-questions page">
+      <div className="page__title">Top Questions</div>
       <QuestionList />
     </div>
   );
