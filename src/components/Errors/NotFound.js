05cc8eee71c7031102083d6b2761d5e8abf1b7a2
Muktar Yusuf <mayunited12@yahoo.com>
2022-11-24 12:40:32 +0100

    imlement back button

--- src/components/Errors/NotFound.js
+++ src/components/Errors/NotFound.js
@@ -1,8 +1,11 @@
 import React from "react";
 import { BiArrowBack } from "react-icons/bi";
+import { useNavigate } from "react-router-dom";
 import "../../Assets/styles/errors.scss"
 
 function NotFound(props) {
+  const navigate =  useNavigate()
+
   return (
     <div className="errors not-found light allChildrenCenter">
       <div className="not-found-layer">
@@ -11,7 +14,7 @@ function NotFound(props) {
           <div className="subtitle">Page not found</div>
           <div className="footnote">The link you clicked may be broken or the page may have been removed or renamed.</div>
           <div className="cta">
-            <button>
+            <button onClick={()=> navigate(-1)}>
               <BiArrowBack className="btn-icon" />
               <span>Back</span>
             </button>
