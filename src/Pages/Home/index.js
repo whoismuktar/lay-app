73c776caaa2c2688afbf78ad81d098c3d0b1341f
Muktar Yusuf <mayunited12@yahoo.com>
2022-11-25 01:58:51 +0100

    dummy data update

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -86,6 +86,138 @@ const mainFeed = [
           profileImage: require("../../Assets/images/conquerer.jpeg"),
         },
       },
+      {
+        comment: {
+          title: "Question is a request to an information",
+        },
+        user: {
+          username: "dexter",
+          firstName: "Jamal",
+          lastName: "Da Conquerer",
+          profileImage: require("../../Assets/images/conquerer.jpeg"),
+        },
+        replies: [
+          {
+            comment: {
+              title: "I really dont know",
+            },
+            user: {
+              username: "princess",
+              firstName: "Castle",
+              lastName: "Bearer",
+              profileImage: null,
+            },
+          },
+          {
+            comment: {
+              title: "I really dont know",
+            },
+            user: {
+              username: "princess",
+              firstName: "Castle",
+              lastName: "Bearer",
+              profileImage: null,
+            },
+          },
+          {
+            comment: {
+              title: "I really dont know",
+            },
+            user: {
+              username: "princess",
+              firstName: "Castle",
+              lastName: "Bearer",
+              profileImage: null,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 1,
+    title: "What is a Question?",
+    description: `Lorem ipsum dolor sit amet <b>consectetur adipisicing elit</b>. 
+      Praesentium ratione, esse id fugit soluta sunt est culpa 
+      deserunt sint reiciendis, voluptate animi molestias 
+      eligendi nobis repellat <em>dolore architecto</em>, unde cum!`,
+    image: "",
+    created_at: new Date(),
+    askedBy: {
+      username: "bisi",
+      firstName: "Bisi",
+      lastName: "Afonja",
+      profileImage: null,
+    },
+    answers: [
+      {
+        comment: {
+          title: "Question is a request to an information",
+        },
+        user: {
+          username: "dexter",
+          firstName: "Jamal",
+          lastName: "Da Conquerer",
+          profileImage: require("../../Assets/images/conquerer.jpeg"),
+        },
+        replies: [
+          {
+            comment: {
+              title: "I really dont know",
+            },
+            user: {
+              username: "princess",
+              firstName: "Castle",
+              lastName: "Bearer",
+              profileImage: null,
+            },
+          },
+          {
+            comment: {
+              title: "I really dont know",
+            },
+            user: {
+              username: "princess",
+              firstName: "Castle",
+              lastName: "Bearer",
+              profileImage: null,
+            },
+          },
+          {
+            comment: {
+              title: "I really dont know",
+            },
+            user: {
+              username: "princess",
+              firstName: "Castle",
+              lastName: "Bearer",
+              profileImage: null,
+            },
+          },
+        ],
+      },
+      {
+        comment: {
+          title: "I really dont know",
+        },
+        user: {
+          username: "princess",
+          firstName: "Castle",
+          lastName: "Bearer",
+          profileImage: null,
+        },
+      },
+      {
+        comment: {
+          title: "I dont know",
+        },
+        user: {
+          username: "mario",
+          firstName: "Mr",
+          lastName: "Warner",
+          profileImage: require("../../Assets/images/conquerer.jpeg"),
+        },
+      },
     ],
   },
   {
