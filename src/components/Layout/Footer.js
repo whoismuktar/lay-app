68f91fb90a53b1e62fd4984e536ac10ae33c9156
Muktar Yusuf <mayunited12@yahoo.com>
2022-11-26 16:51:26 +0100

    add footer component

---
+++ src/components/Layout/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer(props) {
+  return <div className="app-footer" id="footer">
+    <div className="app-footer__content">
+      Footer
+    </div>
+  </div>;
+}
+
+export default Footer;
