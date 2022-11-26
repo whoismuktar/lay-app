1e99e7e4ffa3c7436ed5ab107e833a92f65e0917
Muktar Yusuf <mayunited12@yahoo.com>
2022-11-26 14:19:03 +0100

    implement tag and tag autocomplete

--- src/components/Question/AskQuestion.js
+++ src/components/Question/AskQuestion.js
@@ -1,4 +1,4 @@
-import { Button, Input, Modal, Popconfirm } from "antd";
+import { Button, Input, Modal, Popconfirm, Tag, AutoComplete } from "antd";
 import React, { useEffect, useState } from "react";
 import { Editor } from "react-draft-wysiwyg";
 import editorToolbar from "../../Config/editorToolbar";
@@ -8,17 +8,88 @@ import draftToHtml from "draftjs-to-html";
 import { FiImage } from "react-icons/fi";
 import { MdOutlineEmojiEmotions } from "react-icons/md";
 import { GoMention } from "react-icons/go";
-import { GrCircleInformation } from "react-icons/gr";
-import { IoMdInformationCircleOutline } from "react-icons/io";
+import {
+  IoIosAddCircleOutline,
+  IoMdInformationCircleOutline,
+} from "react-icons/io";
+import { AiFillTags } from "react-icons/ai";
 
 function AskQuestion({ modalActive = false, askUser }) {
   const [isModalActive, setIsModalActive] = useState(!modalActive);
   const [editorActive, setEditorActive] = useState(false);
+  const [addOwnTag, setAddOwnTag] = useState(false);
+  const [questionTags, setQuestionTags] = useState([]);
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState({
-    htmlValue: "<p></p>\n",
+    htmlquery: "<p></p>\n",
     editorState: EditorState.createEmpty(),
   });
+
+  const getTagsBank = () => {
+    const tags = [
+      {
+        title: "Jollof Rice",
+        count: 40000,
+        value: "JollofRice",
+      },
+      {
+        title: "Cuisine",
+        count: 40000,
+        value: "Cuisine",
+      },
+      {
+        title: "Anime",
+        count: 40000,
+        value: "Anime",
+      },
+      {
+        title: "Movies",
+        count: 40000,
+        value: "Movies",
+      },
+      {
+        title: "Politics",
+        count: 40000,
+        value: "Politics",
+      },
+      {
+        title: "Elon Musk",
+        count: 40000,
+        value: "ElonMusk",
+      },
+      {
+        title: "World Cup",
+        count: 40000,
+        value: "WorldCup",
+      },
+      {
+        title: "JavaScript",
+        count: 40000,
+        value: "JavaScript",
+      },
+      {
+        title: "Hollywood",
+        count: 40000,
+        value: "Hollywood",
+      },
+      {
+        title: "Photography",
+        count: 40000,
+        value: "Photography",
+      },
+      {
+        title: "Afrobeats",
+        count: 40000,
+        value: "Afrobeats",
+      },
+    ];
+
+    return tags.map((tag) => ({ ...tag, ...{ label: tag.title } }));
+  };
+  const [tagsBank, setTagsBank] = useState(getTagsBank);
+  const [tagVal, setTagVal] = useState("");
+
+  const onTagValClear = () => setTagVal("");
 
   const placeholder = askUser
     ? `Ask ${askUser.firstName} a question?`
@@ -41,6 +112,18 @@ function AskQuestion({ modalActive = false, askUser }) {
     }
   };
 
+  const onTagSearch = (value) => {
+    setTagVal(value);
+  };
+  const onTagSelect = (value, tag) => {
+    if (!questionTags.includes(value)) {
+      setQuestionTags([...questionTags, tag]);
+      setAddOwnTag(false);
+
+      onTagValClear();
+    }
+  };
+
   // remove any detected linebreak
   useEffect(() => {
     const isLineBreak = title.match(/\n/g);
@@ -48,10 +131,6 @@ function AskQuestion({ modalActive = false, askUser }) {
       setTitle(title.replaceAll("\n", ""));
     }
   }, [title]);
-
-  useEffect(() => {
-    console.log(description);
-  }, [description]);
 
   const TextArea = Input.TextArea;
 
@@ -107,6 +186,7 @@ function AskQuestion({ modalActive = false, askUser }) {
               <span>Ask Emma</span>
             </div>
           )}
+
           <div className="intro-label align-center">
             <span>Ask for a lay answer</span>
             <Popconfirm
@@ -119,6 +199,7 @@ function AskQuestion({ modalActive = false, askUser }) {
               <IoMdInformationCircleOutline className="after-icon" />
             </Popconfirm>
           </div>
+
           <TextArea
             className="ask-question__title"
             maxLength={70}
@@ -132,6 +213,46 @@ function AskQuestion({ modalActive = false, askUser }) {
             onChange={(e) => setTitle(e.target.value)}
             onKeyPress={removeLineBreak}
           />
+        </div>
+
+        <div className="ask-question-tags align-center">
+          <AiFillTags className="before-icon" />
+          {questionTags.map((tag, i) => {
+            return (
+              <span className="ask-question-tag">
+                <Tag closable className="app-tag">
+                  {tag.title}
+                </Tag>
+              </span>
+            );
+          })}
+
+          <span className="ask-question-tag">
+            {addOwnTag && (
+              <AutoComplete
+                options={tagsBank}
+                style={{
+                  width: 100,
+                  height: 27
+                }}
+                onSearch={onTagSearch}
+                onSelect={onTagSelect}
+                placeholder="search tag"
+                value={tagVal}
+                allowClear={onTagValClear}
+                filterOption={(inputValue, tag) =>
+                  tag.value.toUpperCase().indexOf(inputValue.toUpperCase()) !==
+                  -1
+                }
+              />
+            )}
+            {!addOwnTag && questionTags.length < 3 && (
+              <IoIosAddCircleOutline
+                className="dflex cursorPointer"
+                onClick={() => setAddOwnTag(true)}
+              />
+            )}
+          </span>
         </div>
 
         <div className="show-description">
