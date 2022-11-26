294266596cdf73af6d15959f801151e4d5c4c4fd
Muktar Yusuf <mayunited12@yahoo.com>
2022-11-26 16:53:30 +0100

    add auth page

---
+++ src/Pages/Auth/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import AuthCard from "../../components/Auth/AuthCard";
+
+function Login(props) {
+  return <div className="login-pg page">
+    <AuthCard />
+  </div>;
+}
+
+export default Login;
---
+++ src/components/Auth/AuthCard.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Login(props) {
+  const LoginForm = () => {
+    return (
+      <div className="login-card">LOGIN</div>
+    )
+  }
+
+  return <div className="auth-card">
+    <LoginForm />
+  </div>;
+}
+
+export default Login;
