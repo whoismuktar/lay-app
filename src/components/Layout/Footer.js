530b5f64f1132dc41410bf07c181bd73f1d28db7
Muktar Yusuf <mayunited12@yahoo.com>
2022-11-28 15:03:09 +0100

    app footer

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -3,7 +3,9 @@ import React from "react";
 function Footer(props) {
   return <div className="app-footer" id="footer">
     <div className="app-footer__content">
-      Footer
+      <ul>
+        <li>Copyright ©The Lay App | Privacy Policy</li>
+      </ul>
     </div>
   </div>;
 }
